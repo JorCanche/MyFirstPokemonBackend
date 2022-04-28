3246dc62bbbac6a6f732b739f89c4c04231e979a
jorcanche <jecc_0506@gmail.com>
2022-04-28 18:02:31 -0500

    Se realizan los cambios para que se pueda utlizar

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -4,9 +4,9 @@ import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+const hello = async (event) => {
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+    message: `Hello welcome to the exciting Serverless world!`,
     event,
   });
 };
